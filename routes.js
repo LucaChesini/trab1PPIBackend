663bb7488c5d74b5998c084a8c2754f32fad8bce
luca <lucazchesini@gmail.com>
2024-07-05 13:11:19 -0300

    criacao rotas usuarios

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const CardController = require("./controllers/CardController");
+const UsuarioController = require("./controllers/UsuarioController");
 const routes = express.Router();
 
 routes.get('/cards', CardController.findAll);
@@ -7,5 +8,11 @@ routes.post('/cards', CardController.store);
 routes.get('/cards/:id', CardController.show);
 routes.put('/cards/:id', CardController.update);
 routes.delete('/cards/:id', CardController.destroy);
+
+routes.get('/usuarios', UsuarioController.findAll);
+routes.post('/usuarios', UsuarioController.store);
+routes.get('/usuarios/:id', UsuarioController.show);
+routes.put('/usuarios/:id', UsuarioController.update);
+routes.delete('/usuarios/:id', UsuarioController.destroy);
 
 module.exports = routes;
